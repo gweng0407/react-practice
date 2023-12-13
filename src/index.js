111adabb19a020c05d883818382dbdebd377b77f
gweng0407 <152453133+gweng0407@users.noreply.github.com>
2023-12-13 17:12:18 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,16 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-// class Square extends React.Component {
-//     render() {
-//       return (
-//         <button className="square" onClick={() => this.props.onClick({value:'X'})}>
-//           {this.props.value}
-//         </button>
-//       );
-//     }
-//   }
-
 function Square(props) {
   return (
     <button className="square" onClick={props.onClick}>
@@ -21,14 +11,6 @@ function Square(props) {
 }
   
 class Board extends React.Component {
-  // constructor(props){
-  //   super(props);
-  //   this.state = {
-  //     squares: Array(9).fill(null),
-  //     xIsNext: true,
-  //   };
-  // }
-
   renderSquare(i) {
     return (
       <Square 
